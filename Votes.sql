3c2f2192949537b912ff104c7992cb62f03cd946
Naveen2094 <naveenkumarp2094@gmail.com>
2025-02-23 17:37:03 +0530

    Create Votes.sql

---
+++ Votes.sql
@@ -0,0 +1,7 @@
+CREATE TABLE votes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    candidate_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (candidate_id) REFERENCES candidates(id)
+);
